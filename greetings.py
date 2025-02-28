bcf19dfd2c11eb00f2bc5a713b8515a809912edf
Admin <kozevnikovartem320@gmail.com>
2025-02-28 22:57:40 +0300

    28/02

---
+++ greetings.py
@@ -0,0 +1,14 @@
+username = 'Артём Кожевников'
+title = 'Составить меню '
+content = 'На рабочую неделю придумать список блюд'
+status = 'В режиме ожидания'
+created_date = '28-02-2025'
+issue_date = '3-03-2025'
+
+print('Имя пользователя: ', username)
+print('Заголовок заметки: ', title)
+print('Описание заметки: ', content)
+print('Статус заметки: ', status)
+print('Дата создания заметки: ', created_date)
+print('Дата истечения заметки: ', issue_date)
+
